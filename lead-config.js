1b95074264db6f6c2dddaa5d9095104f58ec0ba0
S7EELIX <237071579+S7EELIX@users.noreply.github.com>
2026-06-24 23:47:56 +0530

    Update lead webhook URL

--- lead-config.js
+++ lead-config.js
@@ -1,3 +1,3 @@
 // Paste the Google Apps Script web app URL here after deployment.
 // Email delivery through FormSubmit continues even when this is blank.
-window.REGITRUST_LEAD_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzMzCqRw6jwgve5V7RoyDP4Iywbdkn1N4P75YQ1m3kcTma-ivzHWDwAPTtpTL4tU6cv8Q/exec";
+window.REGITRUST_LEAD_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzet2EfYWGiyGtunBewDiBDI-Ttf5cdcQjKePaLga8O_yUMyStlMs1e0OPd7cU53jf_zw/exec";
